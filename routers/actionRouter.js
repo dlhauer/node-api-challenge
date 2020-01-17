e85c535aa26fc937af9d1e1cb50759d19da88644
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-01-17 10:25:24 -0800

    Validation middleware for actions

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateActionId, (req, res) => {
   Actions.get(req.params.id) 
     .then(action => {
       res.status(200).json(action);
@@ -30,7 +30,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/', validateProjectId, (req, res) => {
+router.post('/', validateAction, validateProjectId, (req, res) => {
   Actions.insert(req.body)
     .then(action => {
       res.status(201).json(action);
@@ -43,7 +43,7 @@ router.post('/', validateProjectId, (req, res) => {
     });
 });
 
-router.put('/:id', validateProjectId, (req, res) => {
+router.put('/:id', validateActionId, validateAction, validateProjectId, (req, res) => {
   const id = req.params.id;
   Actions.update(id, req.body)
     .then(action => {
@@ -57,7 +57,7 @@ router.put('/:id', validateProjectId, (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateActionId, (req, res) => {
   const id = req.params.id;
   Actions.remove(id)
     .then(() => {
@@ -83,6 +83,44 @@ function validateProjectId(req, res, next) {
         });
       }
     });
+}
+
+function validateActionId(req, res, next) {
+  const id = req.params.id;
+  Actions.get(id)
+    .then(action => {
+      if (action) {
+        next();
+      } else {
+        res.status(404).json({
+          message: 'Invalid action ID.'
+        });
+      }
+    });
+}
+
+function validateAction(req, res, next) {
+  if (Object.entries(req.body).length > 0) {
+    if (!req.body.project_id) {
+      res.status(400).json({
+        message: 'Missing required "project_id" field.'
+      });
+    } else if (!req.body.description) {
+      res.status(400).json({
+        message: 'Missing required "description" field.'
+      });
+    } else if (!req.body.notes) {
+      res.status(400).json({
+        message: 'Missing required "notes field.'
+      });
+    } else {
+      next();
+    }
+  } else {
+    res.status(400).json({
+      message: 'Missing action data.'
+    })
+  }
 }
 
 module.exports = router;
